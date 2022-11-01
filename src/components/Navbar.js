cf2337a3d808bd54eee1b779d970aa19a0d30dd8
MindCraftGT <dnyaks32@gmail.com>
2022-11-01 14:03:47 +0300

    general updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+    <nav className='navbar'>
+        <div className='navbar-container'>
+            <i className='fab fa-youtube' />
+        </div>
+    </nav>
+
+    </>
+   
   )
 }
 
